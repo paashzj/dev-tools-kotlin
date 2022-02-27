6097ac9b1e901ebacc5a63e32bfb77eff6b65f61
lovehzj <1922919664@qq.com>
2022-02-27 12:10:33 +0800

    add test coverage (#11)

---
+++ src/main/java/com/github/shoothzj/dev/util/SshUtil.java
@@ -0,0 +1,73 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.github.shoothzj.dev.util;
+
+import com.github.shoothzj.dev.module.SshShellResult;
+import com.github.shoothzj.javatool.util.CommonUtil;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.ByteArrayOutputStream;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class SshUtil {
+
+    public static SshShellResult execCommand(String host, int port,
+                                             String username, String password, String command) throws Exception {
+        final SshShellResult shellResult = new SshShellResult();
+        Session session = null;
+        ChannelExec channel = null;
+        try {
+            session = new JSch().getSession(username, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+
+            channel = (ChannelExec) session.openChannel("exec");
+            channel.setCommand(command);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ByteArrayOutputStream errStream = new ByteArrayOutputStream();
+            channel.setOutputStream(outputStream);
+            channel.setErrStream(errStream);
+            channel.connect();
+            while (!channel.isClosed()) {
+                CommonUtil.sleep(TimeUnit.MILLISECONDS, 100);
+            }
+            shellResult.setOutputContent(outputStream.toString());
+            shellResult.setErrorContent(errStream.toString());
+            shellResult.setReturnCode(channel.getExitStatus());
+            log.info("exec code is {}", shellResult.getReturnCode());
+            log.info("exec err result is {}", shellResult.getErrorContent());
+            log.info("exec output result is {}", shellResult.getOutputContent());
+        } finally {
+            if (session != null) {
+                session.disconnect();
+            }
+            if (channel != null) {
+                channel.disconnect();
+            }
+        }
+        return shellResult;
+    }
+
+}

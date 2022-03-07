53ae2dc9a123f1c9cd80b415e145f352fa032c63
TingTing Wang <1922919664@qq.com>
2022-03-08 07:24:43 +0800

    add lvs trouble shoot message (#53)

---
+++ src/main/kotlin/widget/trouble/lvs/TroubleLvs.kt
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package widget.trouble.lvs
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.unit.sp
+import module.TroubleLvsEnum
+import widget.component.RowPaddingButton
+
+val idx = mutableStateOf(TroubleLvsEnum.VIRTUAL_IP_DETECT)
+
+val editLvsInstanceName = mutableStateOf("")
+val editLvsHost = mutableStateOf("localhost")
+val editLvsPort = mutableStateOf("22")
+val editLvsUsername = mutableStateOf("root")
+val editLvsPassword = mutableStateOf("")
+val editLvsMasterHost = mutableStateOf("")
+val editLvsSlaveHost = mutableStateOf("")
+
+@Composable
+fun TroubleLvs() {
+    Column {
+        Head(idx)
+        when (idx.value) {
+            TroubleLvsEnum.VIRTUAL_IP_DETECT -> {
+                TroubleLvsVirtualIpDetect()
+            }
+        }
+    }
+}
+
+@Composable
+fun Head(idx: MutableState<TroubleLvsEnum>) {
+    Row {
+        Text(R.strings.sceneList, fontSize = 40.sp)
+        RowPaddingButton(
+            onClick = {
+                idx.value = TroubleLvsEnum.VIRTUAL_IP_DETECT
+            },
+        ) {
+            Text(text = R.strings.TroubleSceneLvsVirtualIpDetect)
+        }
+    }
+}
---
+++ src/main/kotlin/widget/trouble/lvs/TroubleLvsVirtualIpDetect.kt
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache license, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package widget.trouble.lvs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import com.github.shoothzj.dev.storage.StorageLvs
+import widget.component.DropdownList
+import widget.config.ConfigItemHost
+import widget.config.ConfigItemPort
+import widget.config.ConfigItemString
+import widget.trouble.TroubleShootBase
+import widget.trouble.nginx.editNginxInstanceName
+
+@Composable
+fun TroubleLvsVirtualIpDetect() {
+    TroubleShootBase(
+        content = {
+            val lvsNameList = StorageLvs.getInstance().listConfigNames()
+            DropdownList(lvsNameList, "lvs ${R.strings.instance}", editNginxInstanceName)
+            ConfigItemHost(
+                editLvsHost,
+                "config host", mutableStateOf("")
+            )
+            ConfigItemPort(
+                editLvsPort,
+                "config port", mutableStateOf("")
+            )
+            ConfigItemString(
+                editLvsUsername,
+                "ssh username", singleLine = true
+            )
+            ConfigItemString(
+                editLvsPassword,
+                "ssh password", singleLine = true
+            )
+            ConfigItemHost(
+                editLvsMasterHost,
+                "lvs master host",
+                mutableStateOf("")
+            )
+            ConfigItemHost(
+                editLvsSlaveHost,
+                "lvs slave host",
+                mutableStateOf("")
+            )
+            if (editLvsInstanceName.value != "") {
+                val lvsConfig = StorageLvs.getInstance().getConfig(editLvsInstanceName.value)
+                editLvsHost.value = lvsConfig.jumpHost
+                editLvsPort.value = lvsConfig.jumpPort.toString()
+                editLvsUsername.value = lvsConfig.jumpUsername
+                editLvsPassword.value = lvsConfig.jumpPassword
+                editLvsMasterHost.value = lvsConfig.masterHost
+                editLvsSlaveHost.value = lvsConfig.slaveHost
+            }
+        },
+        result = {
+        },
+    )
+}

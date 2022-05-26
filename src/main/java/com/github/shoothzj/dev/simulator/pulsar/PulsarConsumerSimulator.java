d479c946b635c2dc4df6a1f1745d964c4c592957
zxJin-x <jinzhixin096@gmail.com>
2022-05-26 22:12:09 +0800

    support pulsar producer

---
+++ src/main/java/com/github/shoothzj/dev/simulator/pulsar/PulsarConsumerSimulator.java
@@ -0,0 +1,88 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.github.shoothzj.dev.simulator.pulsar;
+
+import com.github.shoothzj.javatool.util.ExceptionUtil;
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.SubscriptionInitialPosition;
+import org.apache.pulsar.client.api.SubscriptionType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class PulsarConsumerSimulator {
+
+    private static final Logger log = LoggerFactory.getLogger(PulsarConsumerSimulator.class);
+
+    private PulsarClientSimulator pulsarClientSimulator;
+
+    private static final Integer MAX_RECEIVE_MSG = 100;
+
+    private Consumer<byte[]> consumer;
+
+    public PulsarConsumerSimulator(PulsarClientSimulator pulsarClientSimulator) {
+        this.pulsarClientSimulator = pulsarClientSimulator;
+    }
+
+    public String subscribe(String topic) {
+        try {
+            PulsarClient pulsarClient = pulsarClientSimulator.getPulsarClient();
+            consumer = pulsarClient.newConsumer().topic(topic).subscriptionName(UUID.randomUUID().toString())
+                    .receiverQueueSize(MAX_RECEIVE_MSG).autoUpdatePartitions(true).subscriptionType(SubscriptionType.Failover)
+                    .subscriptionInitialPosition(SubscriptionInitialPosition.Latest)
+                    .subscribe();
+            return "pulsar subscribe success";
+        } catch (PulsarClientException e) {
+            return String.format("pulsar subscribe exception : %s", e.getMessage());
+        }
+    }
+
+    public String receive() {
+        try {
+            Message<byte[]> receive = consumer.receive(100, TimeUnit.MILLISECONDS);
+            if (receive == null) {
+                return "no msg available";
+            }
+            return new String(receive.getValue());
+        } catch (Exception e) {
+            log.warn("consume msg failed. e : {}", ExceptionUtil.getException(e));
+        }
+        return "consume msg failed.";
+    }
+
+    public String close() {
+        try {
+            consumer.close();
+        } catch (PulsarClientException e) {
+            log.error("close producer failed. e : {}", ExceptionUtil.getException(e));
+        }
+        try {
+            pulsarClientSimulator.close();
+        } catch (Exception e) {
+            log.error("close pulsar client failed. e: {}", ExceptionUtil.getException(e));
+        }
+        return "success";
+    }
+}
---
+++ src/main/java/com/github/shoothzj/dev/simulator/pulsar/PulsarProducerSimulator.java
@@ -0,0 +1,82 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.github.shoothzj.dev.simulator.pulsar;
+
+import com.github.shoothzj.javatool.util.ExceptionUtil;
+import org.apache.pulsar.client.api.MessageId;
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.TypedMessageBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+
+public class PulsarProducerSimulator {
+
+    private static final Logger log = LoggerFactory.getLogger(PulsarProducerSimulator.class);
+
+    private String topic;
+
+    private PulsarClientSimulator pulsarClientSimulator;
+
+    private static final Integer MAX_PENDING_MSG = 1000;
+
+    private Producer<byte[]> producer;
+
+    public PulsarProducerSimulator(String topic, PulsarClientSimulator pulsarClientSimulator) {
+        try {
+            this.topic = topic;
+            this.pulsarClientSimulator = pulsarClientSimulator;
+            PulsarClient pulsarClient = pulsarClientSimulator.getPulsarClient();
+            producer = pulsarClient.newProducer().topic(topic).maxPendingMessages(MAX_PENDING_MSG).autoUpdatePartitions(true).create();
+        } catch (PulsarClientException e) {
+            log.error("producer msg failed. e : {}", ExceptionUtil.getException(e));
+        }
+    }
+
+    public String produce(String msg, String key) {
+        try {
+            byte[] bytes = msg.getBytes(StandardCharsets.UTF_8);
+            TypedMessageBuilder<byte[]> messageBuilder = producer.newMessage().value(bytes).key(key);
+            MessageId send = messageBuilder.send();
+            return String.format("send msg success. msgId: %s, topic: %s", send.toString(), topic);
+        } catch (Exception e) {
+            log.warn("produce msg failed. e : {}", ExceptionUtil.getException(e));
+        }
+        return String.format("send msg failed. topic: %s", topic);
+    }
+
+    public String close() {
+        try {
+            producer.close();
+        } catch (PulsarClientException e) {
+            log.error("close producer failed. e : {}", ExceptionUtil.getException(e));
+        }
+
+        try {
+            pulsarClientSimulator.close();
+        } catch (Exception e) {
+            log.error("close pulsar client failed. e: {}", ExceptionUtil.getException(e));
+        }
+        return "success";
+    }
+}
